47043f989977718c60c00a9ca5b09627e81eb686
way2muchnoise <jens.baetens@live.be>
2015-11-25 21:06:51 +0100

    adds disable to the hardcore command, #103

--- src/main/java/hardcorequesting/commands/CommandHardcore.java
+++ src/main/java/hardcorequesting/commands/CommandHardcore.java
@@ -5,6 +5,8 @@ import net.minecraft.command.ICommandSender;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.server.MinecraftServer;
 
+import java.util.List;
+
 public class CommandHardcore extends CommandBase
 {
     public CommandHardcore()
@@ -15,13 +17,28 @@ public class CommandHardcore extends CommandBase
     @Override
     public void handleCommand(ICommandSender sender, String[] arguments)
     {
-        QuestingData.disableVanillaHardcore(sender);
-        if (MinecraftServer.getServer().getEntityWorld().getWorldInfo().isHardcoreModeEnabled())
-            sendChat(sender, "hqm.message.vanillaHardcoreOn");
-        else
-            sendChat(sender, QuestingData.isHardcoreActive() ? "hqm.message.hardcoreAlreadyActivated" : "hqm.message.questHardcore");
-        QuestingData.activateHardcore();
-        if (sender instanceof EntityPlayer)
-            currentLives((EntityPlayer) sender);
+        if (arguments.length == 1 && arguments[0].equalsIgnoreCase("disable")) {
+            QuestingData.disableHardcore();
+            sendChat(sender, "hqm.message.hardcoreDisabled");
+        }else {
+            QuestingData.disableVanillaHardcore(sender);
+            if (MinecraftServer.getServer().getEntityWorld().getWorldInfo().isHardcoreModeEnabled())
+                sendChat(sender, "hqm.message.vanillaHardcoreOn");
+            else
+                sendChat(sender, QuestingData.isHardcoreActive() ? "hqm.message.hardcoreAlreadyActivated" : "hqm.message.questHardcore");
+            QuestingData.activateHardcore();
+            if (sender instanceof EntityPlayer)
+                currentLives((EntityPlayer) sender);
+        }
+    }
+
+    @Override
+    public List<String> addTabCompletionOptions(ICommandSender sender, String[] args) {
+        List<String> list =  super.addTabCompletionOptions(sender, args);
+        if (args[0].isEmpty() || args[0].startsWith("e"))
+            list.add("enable");
+        if (args[0].isEmpty()|| args[0].startsWith("d"))
+            list.add("disable");
+        return list;
     }
 }
