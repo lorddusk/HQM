705f3f923d97dc42b9a3bd7c5b1c526cb25b4cbb
Joe Amenta <airbreather@linux.com>
2015-12-12 07:12:57 -0500

    Revert "Should fix #124 Attempt number 2"
    This reverts commit 90b93cd30cddbb6e38a0226c8147dea78c98c359.

--- src/main/java/hardcorequesting/quests/QuestTaskItems.java
+++ src/main/java/hardcorequesting/quests/QuestTaskItems.java
@@ -355,18 +355,6 @@ public abstract class QuestTaskItems extends QuestTask {
         return items;
     }
 
-    private ItemRequirement[] getFriendlyItems(ItemRequirement[] items) {
-        if (items.length < DataBitHelper.TASK_ITEM_COUNT.getMaximum()) {
-            items = Arrays.copyOf(items, items.length + 1);
-        } else {
-            return items;
-        }
-
-        items[items.length - 1] = new ItemRequirement((ItemStack) null, 1);
-        setPositions(items);
-        return items;
-    }
-
     @SideOnly(Side.CLIENT)
     @Override
     public void onClick(GuiQuestBook gui, EntityPlayer player, int mX, int mY, int b) {
@@ -394,20 +382,12 @@ public abstract class QuestTaskItems extends QuestTask {
                     break;
                 }
             }
-
-        } else {
-            ItemRequirement[] items = getFriendlyItems(this.items);
-
-            for (int i = 0; i < items.length; i++) {
-                ItemRequirement item = items[i];
-                if (gui.isOpBook && GuiScreen.isShiftKeyDown()) {
-                    if (isCompleted(player)) {
-                        resetTask(QuestingData.getUserName(player), i);
-                    } else {
-                        completeTask(QuestingData.getUserName(player), i, item.required);
-                    }
-                    break;
-                }
+        }
+        if (gui.isOpBook && GuiScreen.isShiftKeyDown()) {
+            if (isCompleted(player)) {
+                resetTask(QuestingData.getUserName(player), i);
+            } else {
+                completeTask(QuestingData.getUserName(player), i, item.required);
             }
         }
     }
