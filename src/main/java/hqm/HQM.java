f35244164bcee79193f97b336f327fb2a8296d45
canitzp <canitzp@googlemail.com>
2018-01-11 20:09:50 +0100

    Preparation for the restart

---
+++ src/main/java/hqm/HQM.java
@@ -0,0 +1,15 @@
+package hqm;
+
+import net.minecraftforge.fml.common.Mod;
+
+/**
+ * @author canitzp
+ */
+@Mod(modid = HQM.MODID, name = HQM.MODNAME, version = HQM.MODVERSION)
+public class HQM {
+
+    public static final String MODID = "hqm";
+    public static final String MODNAME = "HardcoreQuestingMode";
+    public static final String MODVERSION = "@VERSION@";
+
+}
