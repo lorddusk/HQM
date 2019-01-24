81717054faa2c09b33a381a492509a24ceb68428
Jon McManus <due@wxwhatever.com>
2019-01-24 10:09:10 +1000

    Resolve dedicated server crash.

--- src/main/java/hardcorequesting/util/HQMUtil.java
+++ src/main/java/hardcorequesting/util/HQMUtil.java
@@ -1,8 +1,10 @@
 package hardcorequesting.util;
 
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.dedicated.DedicatedServer;
 import net.minecraft.server.integrated.IntegratedServer;
 import net.minecraft.world.World;
+import net.minecraftforge.fml.common.FMLCommonHandler;
 
 import javax.annotation.Nonnull;
 
@@ -15,11 +17,11 @@ public class HQMUtil {
     /**
      * A true and save way to determine if a player is actually in a single player only game. No Server nor Integrated Server (LAN World)
      *
-     * @param world The world of the player
      * @return true if the player plays in a real single player world
      */
-    public static boolean isGameSingleplayer(@Nonnull World world) {
-        MinecraftServer server = world.getMinecraftServer();
-        return server == null || (server instanceof IntegratedServer && !(((IntegratedServer) server).getPublic()));
+    public static boolean isGameSingleplayer() {
+        return FMLCommonHandler.instance().getMinecraftServerInstance().isSinglePlayer();
+        // casuses dedicated server crash
+        // return server == null || (server instanceof IntegratedServer && !(((IntegratedServer) server).getPublic()));
     }
 }
