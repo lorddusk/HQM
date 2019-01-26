d7fa6a5fb4dd7fe26b5b8b1b69c26aa59f19a24d
Jon McManus <due@wxwhatever.com>
2019-01-26 14:49:41 +1000

    More work on #449: at some point in time, item precision needs to be considered.

--- src/main/java/hardcorequesting/quests/task/QuestTaskBlock.java
+++ src/main/java/hardcorequesting/quests/task/QuestTaskBlock.java
@@ -10,6 +10,7 @@ import net.minecraft.block.*;
 import net.minecraft.block.properties.IProperty;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
@@ -26,8 +27,11 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.Collection;
 
+import static net.minecraft.block.BlockDoublePlant.EnumPlantType;
+
 public abstract class QuestTaskBlock extends QuestTaskItems {
     public static Method getSilkTouchDrop = null;
+    public static final String NULL_NAME = "item.null.name";
 
     public QuestTaskBlock(Quest parent, String description, String longDescription) {
         super(parent, description, longDescription);
@@ -51,12 +55,16 @@ public abstract class QuestTaskBlock extends QuestTaskItems {
     public static ItemStack itemForState (IBlockState state) {
         Block block = state.getBlock();
 
+        ItemStack res = ItemStack.EMPTY;
+
         if (getSilkTouchDrop != null) {
             try {
-                return (ItemStack) getSilkTouchDrop.invoke(block, state);
+                res = (ItemStack) getSilkTouchDrop.invoke(block, state);
             } catch (ReflectiveOperationException ignored) {
             }
         }
+
+        if (!res.isEmpty()) return res;
 
         return genericForState(state);
     }
@@ -67,6 +75,7 @@ public abstract class QuestTaskBlock extends QuestTaskItems {
         int meta = 0;
 
         Item itemBlock = Item.getItemFromBlock(block);
+        ItemStack res = ItemStack.EMPTY;
         if (itemBlock == Items.AIR) {
             // TODO: This assumption could fail dramaticlaly
             ItemBlockSpecial iBlock = new ItemBlockSpecial(block);
@@ -74,19 +83,30 @@ public abstract class QuestTaskBlock extends QuestTaskItems {
                 meta = block.getMetaFromState(state);
             }
 
-            return new ItemStack(iBlock, 1, meta);
+            res = new ItemStack(iBlock, 1, meta);
         } else {
             if (itemBlock.getHasSubtypes()) {
                 meta = block.getMetaFromState(state);
             }
 
-            return new ItemStack(itemBlock, 1, meta);
+            res = new ItemStack(itemBlock, 1, meta);
         }
-    }
 
-    // TODO: Delete this
-    public static ItemStack getItemDropped (IBlockState state) {
-        return ItemStack.EMPTY;
+        if (res.isEmpty() || res.getDisplayName().equals(NULL_NAME)) {
+            ItemBlock iBlock = new ItemBlock(block);
+            if (iBlock.getHasSubtypes()) {
+                meta = block.getMetaFromState(state);
+            }
+
+            res = new ItemStack(iBlock, 1, meta);
+        }
+
+        if (res.isEmpty() || res.getDisplayName().equals(NULL_NAME)) {
+            // give up in despair
+            return ItemStack.EMPTY;
+        } else {
+            return res;
+        }
     }
 
     @Override
@@ -110,8 +130,20 @@ public abstract class QuestTaskBlock extends QuestTaskItems {
         BlockPos pos = event.getPos();
         Block block = state.getBlock();
 
+        /* Block of manual overrides:
+            These aren't pretty but there's no way around them.
+        */
         if (block instanceof BlockBed) {
+            // Tile entity information needs to be fetched
             drop = ((BlockBed) block).getItem(world, pos, state);
+        } else if (block instanceof BlockCauldron) {
+            // The silkTouch and normal drops aren't sufficient for this
+            drop = new ItemStack(Items.CAULDRON, 1, 0);
+        } else if (block instanceof BlockSkull) {
+            // While the drop provides accurate information,
+            // the skull stored in the item stack does not
+            // have the same item (Blocks.SKULL versus Items.SKULL)
+            drop = ((BlockSkull) block).getItem(world, pos, state);
         }
 
         if (keys.contains(BlockSlab.HALF)) {
@@ -120,21 +152,29 @@ public abstract class QuestTaskBlock extends QuestTaskItems {
             state = state.withProperty(BlockDoor.HALF, BlockDoor.EnumDoorHalf.LOWER);
         } else if (keys.contains(BlockBed.PART)) {
             state = state.withProperty(BlockBed.PART, BlockBed.EnumPartType.HEAD);
+        } else if (keys.contains(BlockDoublePlant.HALF)) {
+            EnumPlantType type = world.getBlockState(pos.down()).getValue(BlockDoublePlant.VARIANT);
+            drop = new ItemStack(Blocks.DOUBLE_PLANT, 1, type.getMeta());
         }
 
         NonNullList<ItemStack> consume = NonNullList.create();
-
-        ItemStack stateStack = itemForState(state);
-
         NonNullList<ItemStack> drops = NonNullList.create();
         block.getDrops(drops, world, pos, state, 0);
         drops.removeIf(ItemStack::isEmpty);
+
+        ItemStack stateStack = itemForState(state);
+
+        if (drops.size() == 0) {
+            state = world.getBlockState(pos.down());
+            block.getDrops(drops, world, pos.down(), state, 0);
+            drops.removeIf(ItemStack::isEmpty);
+        }
 
         if (drop.isEmpty()) {
             for (ItemStack droppedStack : drops) {
                 if (droppedStack.getItem() != stateStack.getItem()) continue;
 
-                if (droppedStack.getMetadata() != stateStack.getMetadata()) continue;
+                if (!stateStack.isItemStackDamageable() && droppedStack.getMetadata() != stateStack.getMetadata()) continue;
 
                 drop = droppedStack;
                 break;
