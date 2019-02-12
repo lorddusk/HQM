0d69340552a83aa2cf547a07a00f960c6c560c6e
Jon McManus <due@wxwhatever.com>
2019-02-12 13:51:07 +1000

    Keep it simple, stupid Nooby! "Fix" the break/place task.
    I'm sure it's about to break again at some point in the near future, but
    for now it seems as though relying on Block.getPickedItem is actually
    viable.

--- src/main/java/hardcorequesting/quests/task/QuestTaskBlock.java
+++ src/main/java/hardcorequesting/quests/task/QuestTaskBlock.java
@@ -10,7 +10,6 @@ import net.minecraft.block.*;
 import net.minecraft.block.properties.IProperty;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
@@ -21,13 +20,9 @@ import net.minecraft.util.NonNullList;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraftforge.event.world.BlockEvent;
-import net.minecraftforge.fml.relauncher.ReflectionHelper;
 
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.Collection;
-
-import static net.minecraft.block.BlockDoublePlant.EnumPlantType;
 
 public abstract class QuestTaskBlock extends QuestTaskItems {
     public static Method getSilkTouchDrop = null;
@@ -124,78 +119,19 @@ public abstract class QuestTaskBlock extends QuestTaskItems {
         // TODO: Unhappy with this solution
         Collection<IProperty<?>> keys = state.getPropertyKeys();
 
-        ItemStack drop = ItemStack.EMPTY;
-
         World world = event.getWorld();
         BlockPos pos = event.getPos();
         Block block = state.getBlock();
 
-        /* Block of manual overrides:
-            These aren't pretty but there's no way around them.
-        */
-        if (block instanceof BlockBed) {
-            // Tile entity information needs to be fetched
-            drop = ((BlockBed) block).getItem(world, pos, state);
-        } else if (block instanceof BlockCauldron) {
-            // The silkTouch and normal drops aren't sufficient for this
-            drop = new ItemStack(Items.CAULDRON, 1, 0);
-        } else if (block instanceof BlockSkull) {
-            // While the drop provides accurate information,
-            // the skull stored in the item stack does not
-            // have the same item (Blocks.SKULL versus Items.SKULL)
-            drop = ((BlockSkull) block).getItem(world, pos, state);
-        }
-
-        if (keys.contains(BlockSlab.HALF)) {
-            state = state.withProperty(BlockSlab.HALF, BlockSlab.EnumBlockHalf.BOTTOM);
-        } else if (keys.contains(BlockDoor.HALF)) {
-            state = state.withProperty(BlockDoor.HALF, BlockDoor.EnumDoorHalf.LOWER);
-        } else if (keys.contains(BlockBed.PART)) {
-            state = state.withProperty(BlockBed.PART, BlockBed.EnumPartType.HEAD);
-        } else if (keys.contains(BlockDoublePlant.HALF)) {
-            EnumPlantType type = world.getBlockState(pos.down()).getValue(BlockDoublePlant.VARIANT);
-            drop = new ItemStack(Blocks.DOUBLE_PLANT, 1, type.getMeta());
-        }
-
         NonNullList<ItemStack> consume = NonNullList.create();
-        NonNullList<ItemStack> drops = NonNullList.create();
-        block.getDrops(drops, world, pos, state, 0);
-        drops.removeIf(ItemStack::isEmpty);
+        ItemStack drop = block.getPickBlock(state, null, world, pos, player);
 
-        ItemStack stateStack = itemForState(state);
-
-        if (drops.size() == 0) {
-            state = world.getBlockState(pos.down());
-            block.getDrops(drops, world, pos.down(), state, 0);
-            drops.removeIf(ItemStack::isEmpty);
-        }
-
-        if (drop.isEmpty()) {
-            for (ItemStack droppedStack : drops) {
-                if (droppedStack.getItem() != stateStack.getItem()) continue;
-
-                if (!stateStack.isItemStackDamageable() && droppedStack.getMetadata() != stateStack.getMetadata()) continue;
-
-                drop = droppedStack;
-                break;
-            }
-
-            if (drops.size() == 1 && stateStack.isEmpty()) {
-                drop = drops.get(0);
-            }
-
-            if (drop.isEmpty()) {
-                if (!stateStack.isEmpty()) {
-                    drop = stateStack;
-                } else {
-                    drop = genericForState(state);
-                }
-            }
-
-            if (drop.isEmpty()) {
-                // wing and a prayer
-                drop = drops.get(0);
-            }
+        /* * * CUSTOM OVERRIDES * * * /
+                 THESE ARE
+                    EVIL
+        /* * * * * * * * * * * * * * */
+        if (state.getBlock() instanceof BlockFlowerPot) {
+            drop = new ItemStack(Items.FLOWER_POT);
         }
 
         for (ItemRequirement ireq : items) {
