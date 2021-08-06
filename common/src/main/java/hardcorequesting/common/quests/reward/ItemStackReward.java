b6a02cc61ab83d32988eb2bca7e57d79fde94361
kirderf1 <kirderf4@gmail.com>
2021-08-06 13:38:27 +0200

    List for quest rewards (#553)
    * Use NonNullList for quest rewards
    
    * Change item stack null checks to isEmpty() checks for quest rewards

--- common/src/main/java/hardcorequesting/common/quests/reward/ItemStackReward.java
+++ common/src/main/java/hardcorequesting/common/quests/reward/ItemStackReward.java
@@ -4,6 +4,6 @@ import net.minecraft.world.item.ItemStack;
 
 public class ItemStackReward extends QuestReward<ItemStack> {
     public ItemStackReward(ItemStack stack) {
-        super(stack);
+        super(stack != null ? stack : ItemStack.EMPTY);
     }
 }
