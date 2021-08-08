lorddusk/HQM

commit 7b001beba9674233e65c56faea56dc8cc452e840

Quest repeat fixes (#552)
* Iterate through all teams, and not just named teams in certain circumstances

* Reset instant-repeatable quests with other repeatables to fix quest data stuck in a completed state

* Change QuestTicker to use game-time from the world to get a world-persistent and synced time unit

* Fix deserialization of RepeatInfo

* Fix cooldown time text and cleanup repeat time text code slightly

* Make tick event listening on forge platform match fabric platform by only listening to tick end

common/src/main/java/hardcorequesting/common/quests/QuestTicker.java
@@ -1,23 +1,22 @@
 package hardcorequesting.common.quests;
 
 import hardcorequesting.common.HardcoreQuestingCore;
+import net.minecraft.world.level.Level;
 
 public class QuestTicker {
     
-    private int hours;
-    private int ticks;
+    private long hours;
     
     public QuestTicker(boolean isClient) {
         if (isClient) {
-            HardcoreQuestingCore.platform.registerOnClientTick(minecraftClient -> tick(true));
-        }
-        HardcoreQuestingCore.platform.registerOnServerTick(minecraftClient -> tick(false));
+            HardcoreQuestingCore.platform.registerOnClientTick(minecraftClient -> tick(minecraftClient.level, true));
+        } else
+            HardcoreQuestingCore.platform.registerOnServerTick(minecraftServer -> tick(minecraftServer.overworld(), false));
     }
     
-    public void tick(boolean isClient) {
-        if (++ticks == 1000) {
-            ticks = 0;
-            hours++;
+    public void tick(Level level, boolean isClient) {
+        if (level != null && level.getGameTime() / 1000 != hours) {
+            hours = level.getGameTime() / 1000;
             if (!isClient) {
                 for (Quest quest : Quest.getQuests().values()) {
                     int total = quest.getRepeatInfo().getDays() * 24 + quest.getRepeatInfo().getHours();
@@ -27,14 +26,15 @@ public class QuestTicker {
                         }
                     } else if (quest.getRepeatInfo().getType() == RepeatType.TIME) {
                         quest.resetOnTime(hours - total);
+                    } else if (quest.getRepeatInfo().getType() == RepeatType.INSTANT) {
+                        quest.resetAll();
                     }
                 }
             }
         }
     }
     
-    
-    public int getHours() {
+    public long getHours() {
         return hours;
     }
 }

common/src/main/java/hardcorequesting/common/io/adapter/QuestAdapter.java
@@ -47,8 +47,8 @@ public class QuestAdapter {
             
             return new RepeatInfo(
                     RepeatType.valueOf(GsonHelper.getAsString(object, TYPE)),
-                    GsonHelper.getAsInt(object, HOURS, 0),
-                    GsonHelper.getAsInt(object, DAYS, 0)
+                    GsonHelper.getAsInt(object, DAYS, 0),
+                    GsonHelper.getAsInt(object, HOURS, 0)
             );
         }
     };

common/src/main/java/hardcorequesting/common/io/adapter/QuestDataAdapter.java
@@ -68,7 +68,7 @@ public class QuestDataAdapter {
                         data.available = in.nextBoolean();
                         break;
                     case TIME:
-                        data.time = in.nextInt();
+                        data.time = in.nextLong();
                         break;
                     case TASKS_SIZE:
                         data.tasks = new QuestDataTask[in.nextInt()];

common/src/main/java/hardcorequesting/common/io/adapter/TeamAdapter.java
@@ -117,7 +117,7 @@ public class TeamAdapter {
     public static void commitInvitesMap() {
         if (invitesMap.size() > 0) {
             Map<UUID, Team> tempMap = new HashMap<>();
-            StreamSupport.stream(TeamManager.getInstance().getTeams().spliterator(), false).filter(Objects::nonNull).forEach(team -> tempMap.put(team.getId(), team));
+            StreamSupport.stream(TeamManager.getInstance().getNamedTeams().spliterator(), false).filter(Objects::nonNull).forEach(team -> tempMap.put(team.getId(), team));
             for (Team team : TeamManager.getInstance().getTeams()) {
                 List<UUID> invites = invitesMap.get(team);
                 if (invites != null)

common/src/main/java/hardcorequesting/common/quests/Quest.java
@@ -1575,7 +1575,7 @@ public class Quest {
         }
     }
     
-    public void resetOnTime(int time) {
+    public void resetOnTime(long time) {
         for (Team team : TeamManager.getInstance().getTeams()) {
             QuestData data = team.getQuestData(getQuestId());
             if (data != null && !data.available && data.time <= time) {

common/src/main/java/hardcorequesting/common/quests/QuestData.java
@@ -13,7 +13,7 @@ public class QuestData {
     public boolean claimed;
     public QuestDataTask[] tasks;
     public boolean available = true;
-    public int time;
+    public long time;
     
     public QuestData(int players) {
         reward = new boolean[players];

common/src/main/java/hardcorequesting/common/quests/QuestLine.java
@@ -156,7 +156,7 @@ public class QuestLine {
                     NetworkManager.sendToPlayer(new QuestLineSyncMessage(questLine), serverPlayer);
             }
             NetworkManager.sendToPlayer(new DeathStatsMessage(side), serverPlayer);
-            NetworkManager.sendToPlayer(new TeamStatsMessage(StreamSupport.stream(questLine.teamManager.getTeams().spliterator(), false)), serverPlayer);
+            NetworkManager.sendToPlayer(new TeamStatsMessage(StreamSupport.stream(questLine.teamManager.getNamedTeams().spliterator(), false)), serverPlayer);
         }
     }
     

common/src/main/java/hardcorequesting/common/quests/QuestingData.java
@@ -230,7 +230,7 @@ public class QuestingData {
         QuestingDataManager manager = QuestingDataManager.getInstance();
         QuestingData data = manager.getQuestingData(player);
         Team team = data.getTeam();
-        if (!team.isSingle() && !Iterables.isEmpty(TeamManager.getInstance().getTeams())) {
+        if (!team.isSingle() && !Iterables.isEmpty(TeamManager.getInstance().getNamedTeams())) {
             team.removePlayer(player);
             if (team.getPlayerCount() == 0) {
                 team.deleteTeam();

commit 6e9b1cdf758ff445ed7a41671a3701e32a44bc2c

Fix server crash in QuestTicker caused by #552

common/src/main/java/hardcorequesting/common/proxies/ClientProxy.java
@@ -16,7 +16,7 @@ public class ClientProxy extends CommonProxy {
     @Override
     public void init() {
         super.init();
-        Quest.clientTicker = new QuestTicker(true);
+        Quest.clientTicker = QuestTicker.initClientTicker();
     }
     
     @Override

common/src/main/java/hardcorequesting/common/proxies/CommonProxy.java
@@ -9,7 +9,7 @@ public class CommonProxy {
     public void initSounds() {}
     
     public void init() {
-        Quest.serverTicker = new QuestTicker(false);
+        Quest.serverTicker = QuestTicker.initServerTicker();
     }
     
     public boolean isClient() {

common/src/main/java/hardcorequesting/common/quests/QuestTicker.java
@@ -1,18 +1,28 @@
 package hardcorequesting.common.quests;
 
 import hardcorequesting.common.HardcoreQuestingCore;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 import net.minecraft.world.level.Level;
 
 public class QuestTicker {
     
     private long hours;
     
-    public QuestTicker(boolean isClient) {
-        if (isClient) {
-            HardcoreQuestingCore.platform.registerOnClientTick(minecraftClient -> tick(minecraftClient.level, true));
-        } else
-            HardcoreQuestingCore.platform.registerOnServerTick(minecraftServer -> tick(minecraftServer.overworld(), false));
+    @Environment(EnvType.CLIENT)
+    public static QuestTicker initClientTicker() {
+        QuestTicker ticker = new QuestTicker();
+        HardcoreQuestingCore.platform.registerOnClientTick(minecraftClient -> ticker.tick(minecraftClient.level, true));
+        return ticker;
     }
+    
+    public static QuestTicker initServerTicker() {
+        QuestTicker ticker = new QuestTicker();
+        HardcoreQuestingCore.platform.registerOnServerTick(minecraftServer -> ticker.tick(minecraftServer.overworld(), false));
+        return ticker;
+    }
+    
+    private QuestTicker() {}
     
     public void tick(Level level, boolean isClient) {
         if (level != null && level.getGameTime() / 1000 != hours) {