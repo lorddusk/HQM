bda24043165d91d9ebf1bf0ad1f24f6cb1192ea1
kirderf1 <kirderf4@gmail.com>
2021-08-24 15:02:07 +0200

    Command reward fixes (#574)
    * Perform command rewards with permission level 2
    
    * Don't send command message to player on success for command rewards
    
    * Change the command source name for command rewards

--- common/src/main/java/hardcorequesting/common/quests/reward/CommandReward.java
+++ common/src/main/java/hardcorequesting/common/quests/reward/CommandReward.java
@@ -1,30 +1,66 @@
 package hardcorequesting.common.quests.reward;
 
+import net.minecraft.commands.CommandSource;
+import net.minecraft.commands.CommandSourceStack;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.TextComponent;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.player.Player;
 
+import java.util.UUID;
+
 public class CommandReward extends QuestReward<CommandReward.Command> {
+    
+    private static final String NAME = "Mod-HQM";
+    private static final int PERMISSION_LEVEL = 2;
     
     public CommandReward(Command command) {
         super(command);
     }
     
-    public void execute(Player player) {
-        getReward().execute(player);
-    }
-    
     public static class Command {
-        private String commandString;
+        private final String commandString;
         
         public Command(String commandString) {
             this.commandString = commandString;
         }
         
         public void execute(Player player) {
-            player.getServer().getCommands().performCommand(player.createCommandSourceStack(), commandString.replaceAll("@p", player.getScoreboardName()));
+            CommandSourceStack sourceStack = new CommandSourceStack(new WrapperCommandSource(player), player.position(), player.getRotationVector(),
+                    player.level instanceof ServerLevel ? (ServerLevel)player.level : null, PERMISSION_LEVEL, NAME, new TextComponent(NAME), player.level.getServer(), player);
+            player.getServer().getCommands().performCommand(sourceStack, commandString);
         }
         
         public String asString() {
             return this.commandString;
+        }
+    }
+    
+    private static class WrapperCommandSource implements CommandSource {
+        private final Player player;
+    
+        private WrapperCommandSource(Player player) {
+            this.player = player;
+        }
+    
+        @Override
+        public void sendMessage(Component component, UUID uuid) {
+        player.sendMessage(component, uuid);
+        }
+    
+        @Override
+        public boolean acceptsSuccess() {
+            return false;
+        }
+    
+        @Override
+        public boolean acceptsFailure() {
+            return true;
+        }
+    
+        @Override
+        public boolean shouldInformAdmins() {
+            return true;
         }
     }
 }
