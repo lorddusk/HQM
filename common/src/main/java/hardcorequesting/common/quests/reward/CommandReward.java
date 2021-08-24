1b08e2cecfbd05d3477f8ed14d6a978c47347013
kirderf1 <kirderf4@gmail.com>
2021-08-24 11:48:09 +0200

    Don't send command message to player on success for command rewards

--- common/src/main/java/hardcorequesting/common/quests/reward/CommandReward.java
+++ common/src/main/java/hardcorequesting/common/quests/reward/CommandReward.java
@@ -1,6 +1,12 @@
 package hardcorequesting.common.quests.reward;
 
+import net.minecraft.commands.CommandSource;
+import net.minecraft.commands.CommandSourceStack;
+import net.minecraft.network.chat.Component;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.player.Player;
+
+import java.util.UUID;
 
 public class CommandReward extends QuestReward<CommandReward.Command> {
     
@@ -18,11 +24,41 @@ public class CommandReward extends QuestReward<CommandReward.Command> {
         }
         
         public void execute(Player player) {
-            player.getServer().getCommands().performCommand(player.createCommandSourceStack().withPermission(PERMISSION_LEVEL), commandString);
+            CommandSourceStack sourceStack = new CommandSourceStack(new WrapperCommandSource(player), player.position(), player.getRotationVector(),
+                    player.level instanceof ServerLevel ? (ServerLevel)player.level : null, PERMISSION_LEVEL, player.getName().getString(), player.getDisplayName(), player.level.getServer(), player);
+            player.getServer().getCommands().performCommand(sourceStack, commandString);
         }
         
         public String asString() {
             return this.commandString;
+        }
+    }
+    
+    private static class WrapperCommandSource implements CommandSource {
+        private final Player player;
+    
+        private WrapperCommandSource(Player player) {
+            this.player = player;
+        }
+    
+        @Override
+        public void sendMessage(Component component, UUID uuid) {
+        player.sendMessage(component, uuid);
+        }
+    
+        @Override
+        public boolean acceptsSuccess() {
+            return false;
+        }
+    
+        @Override
+        public boolean acceptsFailure() {
+            return true;
+        }
+    
+        @Override
+        public boolean shouldInformAdmins() {
+            return true;
         }
     }
 }
