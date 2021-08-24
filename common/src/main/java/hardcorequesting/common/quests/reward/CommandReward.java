4af85d0e3fb2eec77d7c981630331ec5448bd2fd
kirderf1 <kirderf4@gmail.com>
2021-08-24 11:31:48 +0200

    Perform command rewards with permission level 2

--- common/src/main/java/hardcorequesting/common/quests/reward/CommandReward.java
+++ common/src/main/java/hardcorequesting/common/quests/reward/CommandReward.java
@@ -4,23 +4,21 @@ import net.minecraft.world.entity.player.Player;
 
 public class CommandReward extends QuestReward<CommandReward.Command> {
     
+    private static final int PERMISSION_LEVEL = 2;
+    
     public CommandReward(Command command) {
         super(command);
     }
     
-    public void execute(Player player) {
-        getReward().execute(player);
-    }
-    
     public static class Command {
-        private String commandString;
+        private final String commandString;
         
         public Command(String commandString) {
             this.commandString = commandString;
         }
         
         public void execute(Player player) {
-            player.getServer().getCommands().performCommand(player.createCommandSourceStack(), commandString.replaceAll("@p", player.getScoreboardName()));
+            player.getServer().getCommands().performCommand(player.createCommandSourceStack().withPermission(PERMISSION_LEVEL), commandString);
         }
         
         public String asString() {
