74a7feb9b34ad1b585f41a6c4ffe869af260ede1
kirderf1 <kirderf4@gmail.com>
2021-10-12 20:52:56 +0200

    Clear and prepare QuestLine data

--- fabric/src/main/java/hardcorequesting/fabric/HQMTest.java
+++ fabric/src/main/java/hardcorequesting/fabric/HQMTest.java
@@ -1,13 +1,32 @@
 package hardcorequesting.fabric;
 
+import hardcorequesting.common.io.LocalDataManager;
 import hardcorequesting.common.quests.QuestLine;
 import net.fabricmc.fabric.api.gametest.v1.FabricGameTest;
 import net.minecraft.gametest.framework.GameTest;
 import net.minecraft.gametest.framework.GameTestHelper;
 
-public class HQMTest {
+import java.lang.reflect.Method;
+
+public class HQMTest implements FabricGameTest {
     
-    @GameTest(template = FabricGameTest.EMPTY_STRUCTURE)
+    @Override
+    public void invokeTestMethod(GameTestHelper context, Method method) {
+        setupTestQuestData();
+        FabricGameTest.super.invokeTestMethod(context, method);
+    }
+    
+    /**
+     * Initializes quest data used for testing
+     */
+    private void setupTestQuestData() {
+        // Initialize data with an empty data manager
+        LocalDataManager dataManager = new LocalDataManager();
+        QuestLine.reset().loadAll(dataManager, dataManager);
+        
+    }
+    
+    @GameTest(template = EMPTY_STRUCTURE)
     public void questLineExists(GameTestHelper helper) {
         
         if (QuestLine.getActiveQuestLine() == null)
