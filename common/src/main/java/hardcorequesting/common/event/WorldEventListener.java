40891bd042986007ecab64c31c7381e877d402b4
kirderf1 <kirderf4@gmail.com>
2022-02-03 12:21:37 +0100

    Log message when saving hqm world data

--- common/src/main/java/hardcorequesting/common/event/WorldEventListener.java
+++ common/src/main/java/hardcorequesting/common/event/WorldEventListener.java
@@ -6,10 +6,14 @@ import hardcorequesting.common.quests.QuestLine;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.level.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class WorldEventListener {
+    private static final Logger LOGGER = LogManager.getLogger();
+    
     public static void onLoad(ResourceKey<Level> worldRegistryKey, ServerLevel world) {
-        if (!world.isClientSide && world.dimension().equals(Level.OVERWORLD)) {
+        if (!world.isClientSide && worldRegistryKey.equals(Level.OVERWORLD)) {
             QuestLine questLine = QuestLine.reset();
             questLine.loadAll(HardcoreQuestingCore.packManager, FileDataManager.createForWorldData(world.getServer()));
         }
@@ -17,6 +21,7 @@ public class WorldEventListener {
     
     public static void onSave(ServerLevel world) {
         if (!world.isClientSide && world.dimension().equals(Level.OVERWORLD)) {
+            LOGGER.info("Saving HQM world data");
             QuestLine.getActiveQuestLine().save(null, FileDataManager.createForWorldData(world.getServer()));
         }
     }
