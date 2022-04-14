b9a5443b4261d492755efbc60158761f8f61d64b
kirderf1 <kirderf4@gmail.com>
2022-04-14 19:40:28 +0200

    Backport item stack and compound tag adapter from #655

--- common/src/main/java/hardcorequesting/common/io/adapter/MinecraftAdapter.java
+++ common/src/main/java/hardcorequesting/common/io/adapter/MinecraftAdapter.java
@@ -2,6 +2,9 @@ package hardcorequesting.common.io.adapter;
 
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+import com.google.gson.JsonPrimitive;
+import com.google.gson.JsonSyntaxException;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import com.mojang.serialization.Dynamic;
 import com.mojang.serialization.JsonOps;
 import hardcorequesting.common.HardcoreQuestingCore;
@@ -10,34 +13,54 @@ import hardcorequesting.common.util.Fraction;
 import net.minecraft.core.Registry;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.NbtOps;
+import net.minecraft.nbt.TagParser;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.GsonHelper;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.material.Fluid;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 /**
  * Created by lang2 on 10/12/2015.
  */
 public class MinecraftAdapter {
     public static final Adapter<ItemStack> ITEM_STACK = new Adapter<ItemStack>() {
-        private static final String ID = "id";
-        private static final String DAMAGE = "damage";
-        private static final String STACK_SIZE = "amount";
-        private static final String NBT = "nbt";
-        
         @Override
         public JsonElement serialize(ItemStack src) {
             if (src.isEmpty())
                 return nullVal();
             
-            return Dynamic.convert(NbtOps.INSTANCE, JsonOps.INSTANCE, src.save(new CompoundTag()));
+            JsonObject jsonObj = new JsonObject();
+            jsonObj.addProperty("id", Registry.ITEM.getKey(src.getItem()).toString());
+            jsonObj.addProperty("Count", src.getCount());
+            
+            CompoundTag tag = src.getTag();
+            if (tag != null) {
+                jsonObj.add("tag", COMPOUND_TAG.serialize(tag));
+            }
+            
+            return jsonObj;
         }
         
         @Override
+        @NotNull
         public ItemStack deserialize(JsonElement json) {
             if (json.isJsonNull())
                 return ItemStack.EMPTY;
-            return ItemStack.of((CompoundTag) Dynamic.convert(JsonOps.INSTANCE, PatchedNbtOps.INSTANCE, json));
+            else {
+                JsonObject jsonObj = json.getAsJsonObject();
+                Item item = Registry.ITEM.get(new ResourceLocation(GsonHelper.getAsString(jsonObj, "id")));
+                int count = GsonHelper.getAsInt(jsonObj, "Count", 1);
+                ItemStack stack = new ItemStack(item, count);
+                
+                if (jsonObj.has("tag")) {
+                    stack.setTag(COMPOUND_TAG.deserialize(jsonObj.get("tag")));
+                }
+                
+                return stack;
+            }
         }
     };
     public static final Adapter<FluidStack> FLUID = new Adapter<FluidStack>() {
@@ -59,6 +82,29 @@ public class MinecraftAdapter {
             Fluid fluid = Registry.FLUID.get(new ResourceLocation(GsonHelper.getAsString(object, FLUID)));
             Fraction amount = Fraction.fromNbt((CompoundTag) Dynamic.convert(JsonOps.INSTANCE, NbtOps.INSTANCE, object.get(VOLUME)));
             return HardcoreQuestingCore.platform.createFluidStack(fluid, amount);
+        }
+    };
+    
+    // Converting a json element to a nbt tag using dynamic ops is unsafe because json does not cover the type system that nbt has.
+    // Use this adapter as a safer alternative. It stores the entire tag as a json string, ensuring that nbt types are kept.
+    // Imitates the serialization method used by SetNbtFunction.
+    public static final Adapter<CompoundTag> COMPOUND_TAG =  new Adapter<CompoundTag>() {
+        @Override
+        public JsonElement serialize(CompoundTag src) {
+            return new JsonPrimitive(src.toString());
+        }
+    
+        @Override
+        public @Nullable CompoundTag deserialize(JsonElement json) {
+            if (json.isJsonObject()) {   //Backwards-compatibility with HQM-1.18.2-5.10.0 and earlier
+                return (CompoundTag) Dynamic.convert(JsonOps.INSTANCE, PatchedNbtOps.INSTANCE, json);
+            } else {
+                try {
+                    return TagParser.parseTag(GsonHelper.convertToString(json, "tag"));
+                } catch (CommandSyntaxException e) {
+                    throw new JsonSyntaxException(e.getMessage());
+                }
+            }
         }
     };
 }
