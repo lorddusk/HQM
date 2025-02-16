a70b8aba462f9197d3ba6845ea8aba8067ad006a
kirderf1 <kirderf4@gmail.com>
2025-02-16 16:33:56 +0100

    Remove unused command strings

--- common/src/main/java/hardcorequesting/common/commands/CommandStrings.java
+++ common/src/main/java/hardcorequesting/common/commands/CommandStrings.java
@@ -8,15 +8,7 @@ public final class CommandStrings {
     //				COMMAND
     // ======================================
     public static final String COMMAND_PREFIX = PREFIX + "command.";
-    
-    public static final String NOT_FOUND = COMMAND_PREFIX + "notFound";
-    public static final String NO_PERMISSION = COMMAND_PREFIX + "noPermission";
-    public static final String SAVE_FAILED = COMMAND_PREFIX + "saveFailed";
-    public static final String SAVE_SUCCESS = COMMAND_PREFIX + "savedTo";
-    public static final String LOAD_FAILED = COMMAND_PREFIX + "loadFailed";
-    public static final String LOAD_SUCCESS = COMMAND_PREFIX + "loadSuccess";
-    public static final String FILE_NOT_FOUND = COMMAND_PREFIX + "fileNotFound";
-    public static final String QUEST_NOT_FOUND = COMMAND_PREFIX + "questNotFound";
+
     public static final String HELP_START = COMMAND_PREFIX + "info.help.start";
     
     public static final String INFO_SUFFIX = ".info";
